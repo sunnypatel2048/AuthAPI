9fc6b2c8e5beca14af06ff35ad0ccae1216cb566
Sunny Patel <sunnypatel2048@gmail.com>
2023-12-23 20:04:02 +0530

    Added Clock Skew in JWT

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -40,6 +40,7 @@ builder.Services.AddAuthentication(x =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret.....")),
         ValidateAudience = false,
         ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero,
     };
 });
 
